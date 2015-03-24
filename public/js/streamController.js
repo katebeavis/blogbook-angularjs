09340008ad1c42a8a6ec64dbb6232271a8fca420
gabe <GabeMaker@users.noreply.github.com>
2015-03-24 20:42:48 +0000

    hardcoded sample wired to html

--- public/js/streamController.js
+++ public/js/streamController.js
@@ -1,8 +1,12 @@
-var streamController = function($scope) {
-  $scope.helloWorld = function() {
-    console.log("hi");
-  };
+var streamController = function($scope, $resource) {
+  $scope.cards = [card, card2]
 };
+
+var card = { Title: "Hello World", Photo: "https://katebeavis.files.wordpress.com/2015/02/fullsizerender-2.jpg", Text: "lorem ipsum", Date: "2015-02-16T22:41:02+00:00",
+         Link: "https://katebeavis.wordpress.com/2015/02/16/day-eleven-weekend-review/", Author: "Kate" }
+
+var card2 = { Title: "Hello World 2", Photo: "https://katebeavis.files.wordpress.com/2015/02/fullsizerender-2.jpg", Text: "lorem ipsum", Date: "2015-02-16T22:41:02+00:00",
+         Link: "https://katebeavis.wordpress.com/2015/02/16/day-eleven-weekend-review/", Author: "Kate" }
 
 angular
   .module("BlogBook")
--- test/indexSpec.js
+++ test/indexSpec.js
@@ -1,8 +1,17 @@
 describe('blogbook homepage', function() {
-  
+
   it('should have a title', function() {
     browser.get('http://localhost:3000/');
 
     expect(browser.getTitle()).toEqual('BlogBook');
+
   });
+
+  it('should show a blog post', function() {
+    browser.get('http://localhost:3000/');
+    // var list = element.all(by.css('.items li'));
+    // expect(list.get(1).getText()).toBe("blog");
+    expect(element(by.css('li')).getText()).toBe("kaprianofox")
+  });
+
 });
