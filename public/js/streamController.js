f0f3deb4c9a19d466b6c32d089cddd3604d22feb
tommasobratto <tbratto22@gmail.com>
2015-03-25 12:00:14 +0000

    added comments and extracted card code to a separate wordpressCard.js file

--- public/js/streamController.js
+++ public/js/streamController.js
@@ -1,20 +1,13 @@
-var card = { 
-  title: undefined,
-  author: undefined,
-  avatar: undefined,
-  date: undefined,
-  text: undefined,
-  postURL: undefined
- };
-
 var streamController = function($scope, $http) {
+  // setting the card variable as part of the DOM/scope of our AngularJS application
   $scope.card = card;
-
 
   $scope.callAPI = function() {
     $http.get("https://public-api.wordpress.com/rest/v1.1/sites/en.blog.wordpress.com/posts/?number=2&pretty=1")
       .then( function(response) {
+        // getting JSON data from the API URL and storing it in a "sample" variable
         $scope.sample = response.data;
+        // setting the card's properties to the actual JSON data properties
         $scope.card.title = $scope.sample.posts[0].title;
         $scope.card.author = $scope.sample.posts[0].author.name;
         $scope.card.avatar = $scope.sample.posts[0].author.avatar_URL;
@@ -24,12 +17,7 @@ var streamController = function($scope, $http) {
         console.log($scope.card);
       });
     };
-
 };
-
-// mock test card for the index.html file
-// var card2 = { Title: "Hello World 2", Author: "kaprianofox", Photo: "http://katebeavis.files.wordpress.com/2015/02/fullsizerender-2.jpg", Text: "This is my Maker's Blog, it's so amazing, This is my Maker's Blog, it's so amazing, This is my Maker's Blog, it's so amazing, This is my Maker's Blog, it's so amazing, This is my Maker's Blog, it's so amazing. This is my Maker's Blog, it's so amazing, This is my Maker's Blog, it's so amazing, This is my Maker's Blog, it's so amazing, This is my Maker's Blog, it's so amazing, This is my Maker's Blog, it's so amazing. This is my Maker's Blog, it's so amazing, This is my Maker's Blog, it's so amazing, This is my Maker's Blog, it's so amazing, This is my Maker's Blog, it's so amazing, This is my Maker's Blog, it's so amazing. This is my Maker's Blog, it's so amazing, This is my Maker's Blog, it's so amazing, This is my Maker's Blog, it's so amazing, This is my Maker's Blog, it's so amazing, This is my Maker's Blog, it's so amazing. This is my Maker's Blog, it's so amazing, This is my Maker's Blog, it's so amazing, This is my Maker's Blog, it's so amazing, This is my Maker's Blog, it's so amazing, This is my Maker's Blog, it's so amazing.", Date: "2015-02-16T22:41:02+00:00",
-//          Link: "https://katebeavis.wordpress.com/2015/02/16/day-eleven-weekend-review/", Author: "Kate" }
 
 angular
   .module("BlogBook")
---
+++ public/js/wordpressCard.js
@@ -0,0 +1,9 @@
+// Initialised a card variable, then set it as an JSON object with default properties
+var card = { 
+  title: undefined,
+  author: undefined,
+  avatar: undefined,
+  date: undefined,
+  text: undefined,
+  postURL: undefined
+ }
