a73ee69cc920909b0f5f3a07fa86dfb3a68b4577
tommasobratto <tbratto22@gmail.com>
2015-03-25 11:41:34 +0000

    added card properties and updated the view file

--- public/js/streamController.js
+++ public/js/streamController.js
@@ -1,4 +1,11 @@
-var card = { title: undefined}
+var card = { 
+  title: undefined,
+  author: undefined,
+  avatar: undefined,
+  date: undefined,
+  text: undefined,
+  postURL: undefined
+ };
 
 var streamController = function($scope, $http) {
   $scope.card = card;
@@ -8,23 +15,17 @@ var streamController = function($scope, $http) {
     $http.get("https://public-api.wordpress.com/rest/v1.1/sites/en.blog.wordpress.com/posts/?number=2&pretty=1")
       .then( function(response) {
         $scope.sample = response.data;
-        $scope.card.title = $scope.sample.posts[0].title
+        $scope.card.title = $scope.sample.posts[0].title;
+        $scope.card.author = $scope.sample.posts[0].author.name;
+        $scope.card.avatar = $scope.sample.posts[0].author.avatar_URL;
+        $scope.card.date = $scope.sample.posts[0].date;
+        $scope.card.text = $scope.sample.posts[0].content;
+        $scope.card.postURL = $scope.sample.posts[0].URL;
+        console.log($scope.card);
       });
-      console.log(card);
     };
 
 };
-
-
-
-// {
-// title: "sample.posts[0].title",
-// author: "sample.posts[0].author.name",
-// Photo: "http://katebeavis.files.wordpress.com/2015/02/fullsizerender-2.jpg",
-// Text: "This is my Maker's Blog, it's so amazing, This is my Maker's Blog, it's so amazing, This is my Maker's Blog, it's so amazing, This is my Maker's Blog, it's so amazing, This is my Maker's Blog, it's so amazing. This is my Maker's Blog, it's so amazing, This is my Maker's Blog, it's so amazing, This is my Maker's Blog, it's so amazing, This is my Maker's Blog, it's so amazing, This is my Maker's Blog, it's so amazing. This is my Maker's Blog, it's so amazing, This is my Maker's Blog, it's so amazing, This is my Maker's Blog, it's so amazing, This is my Maker's Blog, it's so amazing, This is my Maker's Blog, it's so amazing. This is my Maker's Blog, it's so amazing, This is my Maker's Blog, it's so amazing, This is my Maker's Blog, it's so amazing, This is my Maker's Blog, it's so amazing, This is my Maker's Blog, it's so amazing. This is my Maker's Blog, it's so amazing, This is my Maker's Blog, it's so amazing, This is my Maker's Blog, it's so amazing, This is my Maker's Blog, it's so amazing, This is my Maker's Blog, it's so amazing.",
-// Date: "2015-02-16T22:41:02+00:00",
-// Link: "https://katebeavis.wordpress.com/2015/02/16/day-eleven-weekend-review/", Author: "Kate" }
-
 
 // mock test card for the index.html file
 // var card2 = { Title: "Hello World 2", Author: "kaprianofox", Photo: "http://katebeavis.files.wordpress.com/2015/02/fullsizerender-2.jpg", Text: "This is my Maker's Blog, it's so amazing, This is my Maker's Blog, it's so amazing, This is my Maker's Blog, it's so amazing, This is my Maker's Blog, it's so amazing, This is my Maker's Blog, it's so amazing. This is my Maker's Blog, it's so amazing, This is my Maker's Blog, it's so amazing, This is my Maker's Blog, it's so amazing, This is my Maker's Blog, it's so amazing, This is my Maker's Blog, it's so amazing. This is my Maker's Blog, it's so amazing, This is my Maker's Blog, it's so amazing, This is my Maker's Blog, it's so amazing, This is my Maker's Blog, it's so amazing, This is my Maker's Blog, it's so amazing. This is my Maker's Blog, it's so amazing, This is my Maker's Blog, it's so amazing, This is my Maker's Blog, it's so amazing, This is my Maker's Blog, it's so amazing, This is my Maker's Blog, it's so amazing. This is my Maker's Blog, it's so amazing, This is my Maker's Blog, it's so amazing, This is my Maker's Blog, it's so amazing, This is my Maker's Blog, it's so amazing, This is my Maker's Blog, it's so amazing.", Date: "2015-02-16T22:41:02+00:00",
