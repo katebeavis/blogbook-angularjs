81851ffc73989a9a699b340bf5e87706438b0ed4
Kate <katebeavis84@gmail.com>
2015-03-26 19:59:18 +0000

    Fixed merge conflicts

--- public/js/WPCard.js
+++ public/js/WPCard.js
@@ -1,7 +1,7 @@
 var wrapper = function($http){
   var WPCard = function(blog, index){
     this.initialize = function(){
-      var url = "https://public-api.wordpress.com/rest/v1.1/sites/" + blog + "/posts/?number=1";
+      var url = "https://public-api.wordpress.com/rest/v1.1/sites/" + blog + "/posts/";
       var blogData = $http.get(url);
       var self = this;
 
@@ -11,7 +11,7 @@ var wrapper = function($http){
         self.text = response.data.posts[index].excerpt
         self.link = response.data.posts[index].URL
         self.author = response.data.posts[index].author.nice_name
-        self.photo = response.data.posts[index].featured_image || response.data.posts[index].attachments[Object.keys(response.data.posts[index].attachments)[index]].URL || ""
+        self.photo = response.data.posts[index].featured_image || response.data.posts[index].attachments[Object.keys(response.data.posts[index].attachments)[0]].URL || ""
       });
     };
     this.initialize();
--- public/js/streamController.js
+++ public/js/streamController.js
@@ -6,14 +6,13 @@ var streamController = function($scope, WPCard) {
   $scope.addWPCard = function(blog){
     $scope.cards.push(new WPCard(blog, 0));
     $scope.cards.push(new WPCard(blog, 1));
-    console.log('$scope.cards')
   };
 
  $scope.allPosts = function(blog){
     for(i=0; i<20; i++){
       $scope.cards.push(new WPCard(blog, i));
     }
-  };  
+  };
 
   $scope.fillPage = function(){
     for(i=0; i< $scope.blogs.length; i++){
