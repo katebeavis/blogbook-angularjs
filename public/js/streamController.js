7a4d3b892fe5e3822783adf4feca42b9cc7fa684
Thomas Strothjohann <thomas@strothjohann.net>
2015-03-26 14:33:21 +0000

    Displays Featured image or attachments of wordpress blog.

--- public/js/streamController.js
+++ public/js/streamController.js
@@ -1,7 +1,7 @@
 var streamController = function($scope, WPCard) {
   $scope.newBlog = null;
   $scope.cards = [];
-  $scope.blogs = ['katebeavis.wordpress.com', 'joejknowles.wordpress.com', 'detachedhead.wordpress.com', 'thegeekleapforward.wordpress.com'];
+  $scope.blogs = ['katebeavis.wordpress.com', 'joejknowles.wordpress.com', 'detachedhead.wordpress.com', 'thegeekleapforward.wordpress.com', 'en.blog.wordpress.com'];
 
   $scope.addWPCard = function(blog){
     $scope.cards.push(new WPCard(blog));
