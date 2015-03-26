d7f1bc1014e9aaa8e4ae444a37fa98ef611d8d8e
Thomas Strothjohann <thomas@strothjohann.net>
2015-03-26 00:37:28 +0000

    Wordpress-Factory is working. Added clickable hardcoded wp-bloglist to index.html

---
+++ public/js/WPCard.js
@@ -0,0 +1,24 @@
+var wrapper = function($http){
+  var WPCard = function(blog){
+    this.initialize = function(){
+      var url = "https://public-api.wordpress.com/rest/v1.1/sites/" + blog + "/posts/";
+      var blogData = $http.get(url);
+      var self = this;
+
+      blogData.then(function(response){
+        self.title = response.data.posts[0].title
+        self.date = Date(response.data.posts[0].date);
+        self.text = response.data.posts[0].excerpt
+        self.link = response.data.posts[0].URL
+        self.author = response.data.posts[0].author.nice_name
+        self.photo = response.data.posts[0].featured_image
+      });
+    };
+    this.initialize();
+  };
+  return (WPCard);
+};
+
+angular
+  .module('BlogBook')
+  .factory('WPCard', wrapper)
--- public/js/streamController.js
+++ public/js/streamController.js
@@ -1,23 +1,12 @@
-var streamController = function($scope, $http) {
-  // setting the card variable as part of the DOM/scope of our AngularJS application
-  $scope.card = card;
+var streamController = function($scope, WPCard) {
+  $scope.newBlog = null;
+  $scope.cards = [];
+  $scope.blogs = ['katebeavis.wordpress.com', 'joejknowles.wordpress.com', 'detachedhead.wordpress.com', 'thegeekleapforward.wordpress.com'];
 
-  $scope.callAPI = function() {
-    $http.get("https://public-api.wordpress.com/rest/v1.1/sites/en.blog.wordpress.com/posts/?number=2&pretty=1")
-      .then( function(response) {
-        // getting JSON data from the API URL and storing it in a "sample" variable
-        $scope.sample = response.data;
-        // setting the card's properties to the actual JSON data properties
-        $scope.card.title = $scope.sample.posts[0].title;
-        $scope.card.author = $scope.sample.posts[0].author.name;
-        $scope.card.avatar = $scope.sample.posts[0].author.avatar_URL;
-        $scope.card.date = $scope.sample.posts[0].date;
-        $scope.card.text = $scope.sample.posts[0].content;
-        $scope.card.postURL = $scope.sample.posts[0].URL;
-        console.log($scope.card);
-      });
-    };
-    $scope.callAPI();
+  $scope.addWPCard = function(blog){
+    $scope.cards.push(new WPCard(blog));
+    $scope.newBlog = null;
+  };
 };
 
 angular
