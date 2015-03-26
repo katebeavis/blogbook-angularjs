755f0ba336e9e961b7e787be1b2831bbf69a703a
tommasobratto <tbratto22@gmail.com>
2015-03-26 19:45:03 +0000

    added all posts from a blog feature

--- public/js/WPCard.js
+++ public/js/WPCard.js
@@ -1,17 +1,17 @@
 var wrapper = function($http){
-  var WPCard = function(blog){
+  var WPCard = function(blog, index){
     this.initialize = function(){
       var url = "https://public-api.wordpress.com/rest/v1.1/sites/" + blog + "/posts/";
       var blogData = $http.get(url);
       var self = this;
 
       blogData.then(function(response){
-        self.title = response.data.posts[0].title
-        self.date = response.data.posts[0].date
-        self.text = response.data.posts[0].excerpt
-        self.link = response.data.posts[0].URL
-        self.author = response.data.posts[0].author.nice_name
-        self.photo = response.data.posts[0].featured_image || response.data.posts[0].attachments[Object.keys(response.data.posts[0].attachments)[0]].URL || ""
+        self.title = response.data.posts[index].title
+        self.date = response.data.posts[index].date
+        self.text = response.data.posts[index].excerpt
+        self.link = response.data.posts[index].URL
+        self.author = response.data.posts[index].author.nice_name
+        self.photo = response.data.posts[index].featured_image || response.data.posts[index].attachments[Object.keys(response.data.posts[index].attachments)[index]].URL || ""
       });
     };
     this.initialize();
--- public/js/streamController.js
+++ public/js/streamController.js
@@ -1,12 +1,18 @@
 var streamController = function($scope, WPCard) {
-  $scope.newBlog = null;
   $scope.cards = [];
   $scope.blogs = ['katebeavis.wordpress.com', 'joejknowles.wordpress.com', 'detachedhead.wordpress.com', 'thegeekleapforward.wordpress.com', 'en.blog.wordpress.com'];
 
   $scope.addWPCard = function(blog){
-    $scope.cards.push(new WPCard(blog));
-    $scope.newBlog = null;
+    $scope.cards.push(new WPCard(blog, 0));
+    $scope.cards.push(new WPCard(blog, 1));
+    console.log('$scope.cards')
   };
+
+ $scope.allPosts = function(blog){
+    for(i=0; i<20; i++){
+      $scope.cards.push(new WPCard(blog, i));
+    }
+  };  
 
   $scope.fillPage = function(){
     for(i=0; i< $scope.blogs.length; i++){
