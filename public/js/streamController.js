109094698564bde93a0a4b75b5051a27bfca38c7
Kate <katebeavis84@gmail.com>
2015-03-26 19:42:31 +0000

    Added buttons and filter for authors

--- public/js/streamController.js
+++ public/js/streamController.js
@@ -1,6 +1,7 @@
 var streamController = function($scope, WPCard) {
   $scope.newBlog = null;
   $scope.cards = [];
+  $scope.filters = {}
   $scope.blogs = ['katebeavis.wordpress.com', 'joejknowles.wordpress.com', 'detachedhead.wordpress.com', 'thegeekleapforward.wordpress.com', 'en.blog.wordpress.com'];
 
   $scope.addWPCard = function(blog){
@@ -13,8 +14,13 @@ var streamController = function($scope, WPCard) {
       $scope.addWPCard($scope.blogs[i]);
     }
   };
-  
+
   $scope.fillPage();
+
+
+  $scope.setFilter = function(index){
+    $scope.filters = {author: $scope.cards[index].author}
+  };
 };
 
 angular
