a79f0a42f9c0b652fb1d39ad18afa6714ca5a1b5
tommasobratto <tbratto22@gmail.com>
2015-03-26 14:29:47 +0000

    added show post title test

--- test/indexSpec.js
+++ test/indexSpec.js
@@ -1,15 +1,19 @@
 describe('blogbook homepage', function() {
 
-  it('should have a title', function() {
-    browser.get('http://localhost:3000/');
+  beforeEach( function() {
+    browser.get('http://localhost:3000/')
+  })
 
+  it('should have a title', function() {
     expect(browser.getTitle()).toEqual('BlogBook');
   });
 
   it('should show the most recent post', function() {
-    browser.get('http://localhost:3000/');
+    expect(element(by.css('.date')).getText()).toEqual('on 2015-03-25T10:19:31+00:00');
+  });
 
-    expect(element(by.id('date')).getText()).toEqual('on 2015-03-25T10:19:31+00:00');
+  it('should show the title of the post', function() {
+    expect(element(by.css('.title')).getText()).toEqual('Reflections: Context Switching');
   });
 
 });
